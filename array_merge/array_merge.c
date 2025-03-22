d3e8e088344800ead147a1253b41703347679ce5
TyBeas <beasl110@morris.umn.edu>
2025-03-21 19:44:06 -0500

    Fixing errors

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -1,7 +1,62 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
+#include <stdlib.h>
+
+int totalSize(int num_arrays, int* sizes) {
+int total = 0;
+for (int i = 0; i < num_arrays; i++) {
+    total = (total + sizes[i]);
+}
+return total;
+
+}
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
-  return sizes;
+int size = totalSize(num_arrays, sizes);
+
+if (num_arrays == 0) {
+    int* emptyArray = (int*) calloc(1, sizeof(int));
+    emptyArray[0] = 0;
+    return emptyArray;
+}
+
+int currentArraySize = 0;
+int currentArrayIndex = 0;
+int* temp = (int*) calloc(size, sizeof(int));
+
+for (int i = 0; i < num_arrays; i++) {
+    currentArraySize = sizes[i];
+    for (int j = 0; j < sizes[i]; j++) {
+        temp[currentArrayIndex] = values[i][j];
+        currentArrayIndex++;
+    }
+}
+
+mergesort(currentArrayIndex, temp);
+
+int unique = 1;
+int current = temp[0];
+for (int f = 1; f < size; f++) {
+    if (current != temp[f]) {
+        current = temp[f];
+        unique++;
+    }
+}
+
+int* distinct = (int*) calloc(unique + 1, sizeof(int));
+distinct[0] = unique;
+distinct[1] = temp[0];
+current = temp[0];
+int distinctIndex = 2;
+
+for (int k = 1; k < size; k++) {
+    if (current != temp[k]) {
+        current = temp[k];
+        distinct[distinctIndex] = temp[k];
+        distinctIndex++;
+    }
+}
+
+free(temp);
+return distinct;
 }
--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,52 @@
 #include "mergesort.h"
 
+void mergeRanges(int values[], int start, int mid, int end) {
+int size = end - start + 1;
+int * temp = (int*) calloc(size, sizeof(int));
+int first = start;
+int second = mid + 1;
+int index = 0;
+
+while((first <= mid) && (second <= end)) {
+    if(values[first] < values[second]) {
+        temp[index] = values[first];
+        first++;
+    } else {
+        temp[index] = values[second];
+        second++;
+    }
+    index++;
+}
+
+while(first <= mid) {
+    temp[index] = values[first];
+    first++;
+    index++;
+}
+
+while(second <= end) {
+    temp[index] = values[second];
+    second++;
+    index++;
+}
+
+for(int i = 0; i < size; i++) {
+    values[start + i] = temp[i];
+}
+
+free(temp);
+}
+
+void mergesortRange(int values[], int start, int end) {
+    int size = end - start + 1;
+    if(size > 2) {
+        int mid = (start + end) / 2;
+        mergesortRange(values, start, mid);
+        mergesortRange(values, mid + 1, end);
+        mergeRanges(values, start, mid, end);
+    }
+}
+
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+    mergesortRange(values, 0, size - 1);
 }
