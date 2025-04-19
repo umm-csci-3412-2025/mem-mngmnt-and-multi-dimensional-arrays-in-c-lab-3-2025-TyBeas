265cbfae6efb1368bda4f07162d1a174e5f0f269
TyBeas <beasl110@morris.umn.edu>
2025-04-19 17:57:39 -0500

    Hopefully fixing mergesort.c

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,52 +1,68 @@
 #include "mergesort.h"
 
-void mergeRanges(int values[], int start, int mid, int end) {
-int size = end - start + 1;
-int * temp = (int*) calloc(size, sizeof(int));
-int first = start;
-int second = mid + 1;
-int index = 0;
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
-while((first <= mid) && (second <= end)) {
-    if(values[first] < values[second]) {
-        temp[index] = values[first];
-        first++;
-    } else {
-        temp[index] = values[second];
-        second++;
+void int_mergesort(unsigned int size, int* values);
+
+void merge_arrays(int* dest, unsigned int left_size, int* left, unsigned int right_side, int* right);
+
+void mergesort(int raw_size, int* values) {
+
+    assert(raw_size >= 0);
+    unsigned int size = (unsigned int) raw_size;
+
+    int_mergesort(size, values);
+}
+
+void int_mergesort(unsigned int size, int* values) {
+    if(size <= 1) {
+        return;
     }
-    index++;
+
+    unsigned int left_length = size / 2;
+    unsigned int right_length = size - left_length;
+
+    int* left_side = (int*) malloc(left_length * sizeof(int));
+    int* right_side = (int*) malloc(right_length * sizeof(int));
+
+    memcpy(left_side, values, left_length * sizeof(int));
+    memcpy(right_side, values + left_length, right_length * sizeof(int));
+
+    int_mergesort(left_length, left_side);
+    int_mergesort(right_length, right_side);
+
+    merge_arrays(values, left_length, left_side, right_length, right_side);
+
+    free(left_side);
+    free(right_side);
 }
 
-while(first <= mid) {
-    temp[index] = values[first];
-    first++;
-    index++;
-}
+void merge_arrays(int* dest, unsigned int left_size, int* left, unsigned int right_size, int* right) {
+    unsigned int left_index = 0;
+    unsigned int right_index = 0;
+    unsigned int dest_index = 0;
 
-while(second <= end) {
-    temp[index] = values[second];
-    second++;
-    index++;
-}
+    while (1) {
+        if (left_index >= left_size) {
+            memcpy(dest + dest_index, right + right_index, (right_size - right_index) * sizeof(int));
+            return;
+        }
+        if (right_index >= right_size) {
+            memcpy(dest + dest_index, left + left_index, (left_size - left_index) * sizeof(int));
+            return;
+        }
 
-for(int i = 0; i < size; i++) {
-    values[start + i] = temp[i];
-}
+        int left_value = left[left_index];
+        int right_value = right[right_index];
 
-free(temp);
-}
-
-void mergesortRange(int values[], int start, int end) {
-    int size = end - start + 1;
-    if(size > 2) {
-        int mid = (start + end) / 2;
-        mergesortRange(values, start, mid);
-        mergesortRange(values, mid + 1, end);
-        mergeRanges(values, start, mid, end);
+        if (left_value < right_value) {
+            dest[dest_index++] = left_value;
+            left_index++;
+        } else {
+            dest[dest_index++] = right_value;
+            right_index++;
+        }
     }
-}
-
-void mergesort(int size, int values[]) {
-    mergesortRange(values, 0, size - 1);
 }
