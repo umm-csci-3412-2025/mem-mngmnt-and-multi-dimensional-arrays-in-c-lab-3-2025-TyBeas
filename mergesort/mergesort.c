8d231abe8f6eb303b8bacc5cc70c8bd1f13e0141
TyBeas <beasl110@morris.umn.edu>
2025-05-01 12:41:59 -0500

    Adding comments

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -4,10 +4,12 @@
 #include <assert.h>
 #include <string.h>
 
+// internal function to sort an array using mergesort
 void int_mergesort(unsigned int size, int* values);
 
 void merge_arrays(int* dest, unsigned int left_size, int* left, unsigned int right_side, int* right);
 
+// Sort arrays with size and values
 void mergesort(int raw_size, int* values) {
 
     assert(raw_size >= 0);
@@ -16,35 +18,41 @@ void mergesort(int raw_size, int* values) {
     int_mergesort(size, values);
 }
 
+// Internal function to sort an array using mergesort
 void int_mergesort(unsigned int size, int* values) {
     if(size <= 1) {
         return;
     }
 
+    // Compute the size of the left and right arrays
     unsigned int left_length = size / 2;
     unsigned int right_length = size - left_length;
 
     int* left_side = (int*) calloc(left_length, sizeof(int));
     int* right_side = (int*) calloc(right_length, sizeof(int));
 
+    // Split the array into two halves
     memcpy(left_side, values, left_length * sizeof(int));
     memcpy(right_side, values + left_length, right_length * sizeof(int));
 
     int_mergesort(left_length, left_side);
     int_mergesort(right_length, right_side);
 
+    // Merge the sorted halves back into the original array
     merge_arrays(values, left_length, left_side, right_length, right_side);
 
     free(left_side);
     free(right_side);
 }
 
+// Merge two sorted arrays into a single sorted array
 void merge_arrays(int* dest, unsigned int left_size, int* left, unsigned int right_size, int* right) {
     unsigned int left_index = 0;
     unsigned int right_index = 0;
     unsigned int dest_index = 0;
 
     while (1) {
+        // Check if we have reached the end of either array
         if (left_index >= left_size) {
             memcpy(dest + dest_index, right + right_index, (right_size - right_index) * sizeof(int));
             return;
@@ -54,6 +62,7 @@ void merge_arrays(int* dest, unsigned int left_size, int* left, unsigned int rig
             return;
         }
 
+        // Compare the current elements of both arrays
         int left_value = left[left_index];
         int right_value = right[right_index];
 
